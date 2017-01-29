2ad8eaa891b7cde0a4f4f2eef6940ffef9b29f99
Noah-Franks <nfranks@purdue.edu>
2017-01-28 19:16:22 -0500

    A python script has been added that counts and graphs the frequencies of characters present in a file. The graphs are dependent on console size at the time of execution.

--- CountCharacters.py
+++ CountCharacters.py
@@ -1,11 +1,14 @@
 import os
+import shutil
 
-filePath = input()
+filePath = input("Please provide a file to analyze\n")
 
 print(filePath)
 
 
-letterSpace = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letterSpace  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+letterSpace += "0123456789"
+letterSpace += "!@#$%^&*()[]{}/?=+\|-_;:'\",.<>`~\n"
 letters = []
 for letter in letterSpace :
     pair = []
@@ -13,11 +16,32 @@ for letter in letterSpace :
     pair.append(0)
     letters.append(pair)
 
-for letter in letters :
-    print(letter)
 
-'''
+
 with open(filePath) as fileobj:
     for word in fileobj:  
-       for ch in word: 
-           print (ch);'''
+       for ch in word: # A character by character read of the file
+
+           chMatchFound = False
+           for l in range(len(letterSpace)) :
+               if letterSpace[l] == ch :
+                   letters[l][1] += 1
+                   chMatchFound = True
+                   break
+           if not chMatchFound :
+               print("Unmatched Letter Found " + ch)
+            
+consoleWidth = shutil.get_terminal_size().columns - 12
+
+for letter in letters :
+    if letter[1] is not 0 :
+        bar = ""
+        if letter[1] < consoleWidth :
+            for i in range(letter[1]) :
+                bar += "#"
+        else :
+            for i in range(consoleWidth) :
+                bar += "#"
+        print(repr(letter[0]) + " " + bar + " " + str(letter[1]))
+        
+
